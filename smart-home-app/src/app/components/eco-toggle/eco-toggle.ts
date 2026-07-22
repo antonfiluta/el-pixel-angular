3d3981bfc9343857b1309593ee50cd23ede2f1dd
antonfiluta <antonfiluta2.0@mail.ru>
2026-07-22 13:47:57 +0300

    feat: create basic components

---
+++ smart-home-app/src/app/components/eco-toggle/eco-toggle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eco-toggle',
+  imports: [],
+  templateUrl: './eco-toggle.html',
+  styleUrl: './eco-toggle.css',
+})
+export class EcoToggle {}
---
+++ smart-home-app/src/app/components/smart-card/smart-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-card',
+  imports: [],
+  templateUrl: './smart-card.html',
+  styleUrl: './smart-card.css',
+})
+export class SmartCard {}
